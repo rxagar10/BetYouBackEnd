c4161c6bcff35d383dfa15ba46e01e5706c14241
rxagar10 <rxagar10@gmail.com>
2021-07-23 16:08:00 -0400

    added createBet routes

--- api/routes.js
+++ api/routes.js
@@ -1,6 +1,4 @@
-
-
-module.exports = async function({ app }) {
+module.exports = async function ({app}) {
   app.post("/signup", (req, res) => {
     const username = req.body.username;
     const email = req.body.email;
@@ -30,5 +28,31 @@ module.exports = async function({ app }) {
       betsLost: 2,
       betsWitnessed: 8,
     })
+  })
+
+  app.post("/createBet", (req, res) => {
+    const username = req.body.username;
+
+    res.send({
+      myFriends: ["Rishi", "Nate", "BetYou"]
+    })
+  })
+
+  app.post("/submitBet", (req, res) => {
+    const username = req.body.username;
+    const friendBet = req.body.friendBet
+    const witness = req.body.witness
+    const betTitle = req.body.betTitle
+    const terms = req.body.terms
+    const amount = req.body.amount
+    const friendAmount = req.body.friendAmount
+    const settleDate = req.body.settleDate
+
+    console.log(settleDate, witness, friendBet)
+
+    res.send({
+      betSubmitMessage: "success",
+    })
+
   })
 }
