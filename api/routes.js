1f57e67be519def4aa8e29f8a2f515a54f8318da
rxagar10 <rxagar10@gmail.com>
2021-07-26 15:57:26 -0400

    myBets page routes

--- api/routes.js
+++ api/routes.js
@@ -118,4 +118,155 @@ module.exports = async function ({app}) {
       allUsers: allUsers.filter(user => user.username !== friend.username)
     })
   })
+
+  app.post("/myBets", (req, res) => {
+    const username = req.body.username;
+
+    const currentBets = [
+      {title: "Bet1",
+        terms: "These are terms",
+        betFriend: { firstName: "Joe", lastName: "Smith", username: "joesmith" },
+        amount: 5,
+        friendAmount: 5,
+        witness: [],
+        settleDate: "07/31/2020",
+        betId: 1
+      },
+      {title: "Bet2",
+        terms: "These are terms 2",
+        betFriend: { firstName: "Jane", lastName: "Doe", username: "janedoe" },
+        amount: 15,
+        friendAmount: 10,
+        witness: [
+          { firstName: "Nate", lastName: "Kirschner", username: "natekirschner" },
+          { firstName: "Rishi", lastName: "Agarwal", username: "rishiagarwal" }
+        ],
+        settleDate: "08/31/2020",
+        betId: 2
+      }
+    ]
+
+    const pastBets = [
+      {title: "Bet past1",
+        terms: "These are terms",
+        betFriend: { firstName: "Joe", lastName: "Smith", username: "joesmith" },
+        amount: 533,
+        friendAmount: 533,
+        witness: [],
+        settleDate: "07/31/2020",
+        betId: 5
+      },
+      {title: "bet past2",
+        terms: "These are terms 2",
+        betFriend: { firstName: "Jane", lastName: "Doe", username: "janedoe" },
+        amount: 1533,
+        friendAmount: 1033,
+        witness: [
+          { firstName: "Nate", lastName: "Kirschner", username: "natekirschner" },
+          { firstName: "Rishi", lastName: "Agarwal", username: "rishiagarwal" }
+        ],
+        settleDate: "08/3/2020",
+        betId: 4
+      }
+    ]
+
+    const betRequests = [
+      {title: "Bet request 1",
+        terms: "These are terms",
+        betFriend: { firstName: "Joe", lastName: "Smith", username: "joesmith" },
+        amount: 544,
+        friendAmount: 544,
+        witness: [],
+        settleDate: "07/31/2020",
+        betId: 10
+      },
+      {title: "Bet request 2",
+        terms: "These are terms 2",
+        betFriend: { firstName: "Jane", lastName: "Doe", username: "janedoe" },
+        amount: 15,
+        friendAmount: 10,
+        witness: [
+          { firstName: "Nate", lastName: "Kirschner", username: "natekirschner" },
+          { firstName: "Rishi", lastName: "Agarwal", username: "rishiagarwal" }
+        ],
+        settleDate: "08/31/2020",
+        betId: 11
+      }
+    ]
+
+    res.send(
+        {
+          currentBets,
+          pastBets,
+          betRequests
+        }
+    )
+  })
+
+  app.post("/handleBetRequest", (req, res) => {
+    const username = req.body.username
+    const betId = req.body.betId
+    const status = req.body.status
+
+    const currentBets = [
+      {title: "Bet1",
+        terms: "These are terms",
+        betFriend: { firstName: "Joe", lastName: "Smith", username: "joesmith" },
+        amount: 5,
+        friendAmount: 5,
+        witness: [],
+        settleDate: "07/31/2020",
+        betId: 1
+      },
+      {title: "Bet2",
+        terms: "These are terms 2",
+        betFriend: { firstName: "Jane", lastName: "Doe", username: "janedoe" },
+        amount: 15,
+        friendAmount: 10,
+        witness: [
+          { firstName: "Nate", lastName: "Kirschner", username: "natekirschner" },
+          { firstName: "Rishi", lastName: "Agarwal", username: "rishiagarwal" }
+        ],
+        settleDate: "08/31/2020",
+        betId: 2
+      }
+    ]
+
+    const betRequests = [
+      {title: "Bet request 1",
+        terms: "These are terms",
+        betFriend: { firstName: "Joe", lastName: "Smith", username: "joesmith" },
+        amount: 544,
+        friendAmount: 544,
+        witness: [],
+        settleDate: "07/31/2020",
+        betId: 10
+      },
+      {title: "Bet request 2",
+        terms: "These are terms 2",
+        betFriend: { firstName: "Jane", lastName: "Doe", username: "janedoe" },
+        amount: 15,
+        friendAmount: 10,
+        witness: [
+          { firstName: "Nate", lastName: "Kirschner", username: "natekirschner" },
+          { firstName: "Rishi", lastName: "Agarwal", username: "rishiagarwal" }
+        ],
+        settleDate: "08/31/2020",
+        betId: 11
+      }
+    ]
+
+    if(status === "accept") {
+      res.send({
+        currentBets: [...currentBets, betRequests.find((bet) => bet.betId === betId)],
+        betRequests: betRequests.filter((bet) => bet.betId !== betId)
+      })
+    } else {
+      res.send({
+        currentBets,
+        betRequests: betRequests.filter((bet) => bet.betId !== betId)
+      })
+    }
+  })
+
 }
